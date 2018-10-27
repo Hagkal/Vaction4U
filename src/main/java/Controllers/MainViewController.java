18d38cee11791c1e0e8aeb4688066f98ff6d5fd2
omrinao <omrinao@post.bgu.ac.il>
2018-10-27 16:28:27 +0300

    Forms updated and linked to primary stage

--- src/main/java/Controllers/MainViewController.java
+++ src/main/java/Controllers/MainViewController.java
@@ -1,15 +1,20 @@
 package Controllers;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.IOException;
 
 public class MainViewController {
 
     @FXML
     public BorderPane lyt_mainPane;
+    public Button btn_exit;
 
     /**
      * Functions that presents the correct form on the primary stage by clicking the mouse.
@@ -35,5 +40,10 @@ public class MainViewController {
     public void setDelete(MouseEvent mouseEvent) throws IOException {
         FXMLLoader loader = new FXMLLoader();
         lyt_mainPane.setCenter(loader.load(getClass().getResourceAsStream("/fxml/deleteXML.fxml")));
+    }
+
+    public void setExit (MouseEvent mouseEvent) throws IOException {
+        Stage stage = (Stage) btn_exit.getScene().getWindow();
+        stage.close();
     }
 }
