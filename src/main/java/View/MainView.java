f9e15e8f1b85c26787c9704103402511a9ae6fce
omrinao <omrinao@post.bgu.ac.il>
2018-10-29 22:09:52 +0200

    - changed the project completely

--- src/main/java/View/MainView.java
+++ src/main/java/View/MainView.java
@@ -2,15 +2,12 @@ package View;
 import Controller.MainController;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class MainView {
@@ -41,6 +38,20 @@ public class MainView {
     public TextField tf_usernamedelete;
     //delete form label
     public Label lbl_userNameDeleteErr;
+    //update form controllers
+    public TextField tf_usernameUpdate;
+    public TextField tf_firstnameUpdate;
+    public DatePicker dp_dateUpdate;
+    public TextField tf_lastnameUpdate;
+    public TextField tf_hometownUpdate;
+    public TextField tf_passwordUpdate;
+    //update error label
+    public Label lbl_userNameUpdateErr;
+    public Label lbl_firstNameUpdateErr;
+    public Label lbl_lastNameUpdateErr;
+    public Label lbl_passwordUpdateErr;
+    public Label lbl_hometownUpdateErr;
+    public Label lbl_dateUpdateErr;
 
 
     /**
@@ -59,16 +70,22 @@ public class MainView {
     public void setRead(MouseEvent mouseEvent) throws IOException {
         FXMLLoader loader = new FXMLLoader();
         lyt_mainPane.setCenter(loader.load(getClass().getResourceAsStream("/fxml/readXML.fxml")));
+        MainView m = loader.getController();
+        m.set_controller(m_controller);
     }
 
     public void setUpdate(MouseEvent mouseEvent) throws IOException {
         FXMLLoader loader = new FXMLLoader();
         lyt_mainPane.setCenter(loader.load(getClass().getResourceAsStream("/fxml/updateXML.fxml")));
+        MainView m = loader.getController();
+        m.set_controller(m_controller);
     }
 
     public void setDelete(MouseEvent mouseEvent) throws IOException {
         FXMLLoader loader = new FXMLLoader();
         lyt_mainPane.setCenter(loader.load(getClass().getResourceAsStream("/fxml/deleteXML.fxml")));
+        MainView m = loader.getController();
+        m.set_controller(m_controller);
     }
 
     public void setExit (MouseEvent mouseEvent) throws IOException {
@@ -120,8 +137,10 @@ public class MainView {
         }
 
         //date check
-
-
+        if (!isBiggerThen18(dp_date)){
+            lbl_dateerr.setVisible(true);
+            allChecked = false;
+        }
 
         //hometown check
         if (hometown.length() < 2){
@@ -153,7 +172,69 @@ public class MainView {
     }
 
     public void send_update(MouseEvent mouseEvent)throws IOException{
+        ArrayList<String> toSend = new ArrayList<String>();
+        boolean allChecked = true;
+        //temp string fields to get the text from the text field, will be checked before inserting to array list
+        String username = tf_usernameUpdate.getText(),
+                password = tf_passwordUpdate.getText(),
+                firstname = tf_firstnameUpdate.getText(),
+                lastname = tf_lastnameUpdate.getText(),
+                hometown = tf_hometownUpdate.getText();
 
+        //set error labels to be not visible
+        lbl_userNameUpdateErr.setVisible(false);
+        lbl_firstNameUpdateErr.setVisible(false);
+        lbl_lastNameUpdateErr.setVisible(false);
+        lbl_passwordUpdateErr.setVisible(false);
+        lbl_hometownUpdateErr.setVisible(false);
+        lbl_dateUpdateErr.setVisible(false);
+
+
+        //username check
+        if (username.length() == 0){
+            lbl_userNameUpdateErr.setVisible(true);
+            allChecked = false;
+        }
+
+        //password check
+        if (password.length() < 8){
+            lbl_passwordUpdateErr.setVisible(true);
+            allChecked = false;
+        }
+
+        //first name check
+        if (firstname.length() < 2){
+            lbl_firstNameUpdateErr.setVisible(true);
+            allChecked = false;
+        }
+
+        //last name check
+        if (lastname.length() < 2){
+            lbl_lastNameUpdateErr.setVisible(true);
+            allChecked = false;
+        }
+
+        //date check
+        if (!isBiggerThen18(dp_dateUpdate)){
+            lbl_dateUpdateErr.setVisible(true);
+            allChecked = false;
+        }
+
+        //hometown check
+        if (hometown.length() < 2){
+            lbl_hometownUpdateErr.setVisible(true);
+            allChecked = false;
+        }
+
+        if (allChecked){
+            toSend.add(username);
+            toSend.add(password);
+            toSend.add(dp_dateUpdate.getEditor().getText());
+            toSend.add(firstname);
+            toSend.add(lastname);
+            toSend.add(hometown);
+            m_controller.update_user(username, toSend);
+        }
     }
 
     public void send_delete(MouseEvent mouseEvent)throws IOException{
@@ -168,16 +249,52 @@ public class MainView {
     }
 
     public void create_response (String response){
-
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("CREATE USER");
+        alert.setContentText(response);
+        alert.showAndWait();
     }
+
     public void read_response (ArrayList<String> response){
 
     }
+
     public void update_response (String response){
-
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("UPDATE USER");
+        alert.setContentText(response);
+        alert.showAndWait();
     }
-    public void delete_response (String response){
 
+    public void delete_response (String response){
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("DELETE USER");
+        alert.setContentText(response);
+        alert.showAndWait();
+    }
+
+    /**
+     * this method checks if a user age is above 18
+     * @param age
+     * @return
+     */
+    private boolean isBiggerThen18 (DatePicker age) {
+        LocalDate Date = age.getValue();
+        LocalDate today = LocalDate.now();
+        if (Date.getYear() + 18 > today.getYear())
+            return false;
+        else if (Date.getYear() + 18 == today.getYear()) {
+            if (Date.getMonthValue() > today.getMonthValue())
+                return false;
+            else if (Date.getMonthValue() == today.getMonthValue()) {
+                if (Date.getDayOfMonth() > today.getDayOfMonth())
+                    return false;
+                else
+                    return true;
+            }
+        }
+
+        return true;
     }
 
     public void set_controller(MainController c) {
